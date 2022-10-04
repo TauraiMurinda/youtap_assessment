ed464289d81b403df99a7ec10e121aae59d3e9f9
kkk <kkk@kkk-Latitude-E5550>
2022-10-04 01:43:48 -0400

    initial commit

---
+++ src/test/java/com/youtap/microservice/assessment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtap.microservice.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
